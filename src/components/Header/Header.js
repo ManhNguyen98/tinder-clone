a6f03489523680736895276190bdb08a553c268a
ManhNguyen98 <manhnt1@hblab.vn>
2020-11-09 16:12:19 +0700

    Renew UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,20 +6,20 @@ import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
   return (
-    <div className="header">
+    <div className='header'>
       <IconButton>
-        <PersonIcon fontSize="large" className="header__icon"/>
+        <PersonIcon fontSize='small' className='header__icon' />
       </IconButton>
       <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-        alt="tinder"
+        className='header__logo'
+        src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
+        alt='tinder'
       />
       <IconButton>
-        <ForumIcon fontSize="large" className="header__icon" />
+        <ForumIcon fontSize='small' className='header__icon' />
       </IconButton>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
